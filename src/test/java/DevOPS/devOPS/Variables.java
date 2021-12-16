57c8b0bc807506f660df01dc1d6cf5b7c75b1fa0
shubhamag145 <78260963+shubhamag145@users.noreply.github.com>
2021-12-16 19:25:59 +0530

    removing lines

--- src/test/java/DevOPS/devOPS/Variables.java
+++ src/test/java/DevOPS/devOPS/Variables.java
@@ -1,10 +1,8 @@
 package DevOPS.devOPS;
 
-public class Variables {
-	
+public class Variables {	
 	public static final String URL = System.getProperty("url");
 	public static final String UserName =System.getProperty("usr");
 	public static final String Password =System.getProperty("pass");
 	public static final String ScreenShotPath =System.getProperty("sShotPath");
-
 }
